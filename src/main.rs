5ed1be911eec0f925e2f4d517b8c9ca198e28f9b
yikezawa <y.ikezawa93@gmail.com>
2020-09-27 23:15:28 +0900

    mod

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,7 @@ async fn add_todo(
     params: web::Form<AddParams>,
     db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
 ) -> Result<HttpResponse, MyError>{
+    println!("add func is invoked");
     let conn = db.get()?;
     conn.execute("INSERT INTO todo (text) VALUES (?)", &[&params.text])?;
     Ok(HttpResponse::SeeOther()
@@ -59,7 +60,11 @@ async fn delete_todo(
     params: web::Form<DeleteParams>,
     db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
 ) -> Result<HttpResponse, MyError>{
-
+    let conn = db.get()?;
+    conn.execute("DELETE FROM todo WHERE id=?", &[params.id])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
 }
 
 #[get("/")]
@@ -98,7 +103,11 @@ async fn main() -> Result<(), actix_web::Error> {
     )
         .expect("Failed to create a table `todo`.");
 
-    HttpServer::new(move || App::new().service(index).data(pool.clone()))
+    HttpServer::new(move || App::new().
+        service(index)
+        .service(add_todo)
+        .service(delete_todo)
+        .data(pool.clone()))
         .bind("0.0.0.0:8080")?
         .run()
         .await?;
